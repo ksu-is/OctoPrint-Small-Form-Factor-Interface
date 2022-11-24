9723f73cf30d899a814ac663f6182b642cd94825
rohitgibson <89996347+rohitgibson@users.noreply.github.com>
2022-11-23 19:24:27 -0500

    Created datamodels.py
    For ease of data model retrieval (mostly for future development).

---
+++ datamodels.py
@@ -0,0 +1,23 @@
+class DataModels:
+    def ext_data():
+        return {
+            'ext_temp_actual': '',
+            'ext_temp_target': '',
+            'ext_temp_status': '',
+            'ext_temp_state': ''
+        }
+    
+    def bed_data():
+        return {
+            'bed_temp_actual': '',
+            'bed_temp_target': '',
+            'bed_temp_status': '',
+            'bed_temp_state': ''
+        }
+    
+    def job_data():
+        return {
+            'job_time': '',
+            'job_progress': '',
+            'job_state': ''
+        }
