875fddced2537e3e853168c7c798e3c2914c4d35
Rohit Gibson <rgibso50@students.kennesaw.edu>
2022-12-08 17:48:10 -0500

    Added files via upload
    Quality of life improvements for button inputs (dynamically adapting to printer_status) and added job ETA & progress bar

--- htmlbits.py
+++ htmlbits.py
@@ -4,8 +4,8 @@ activity_state_3 = ["Starting", "Printing", "Operational", "Paused"]
 activity_state_4 = ["Complete", "Stopped"]
 
 class Buttons:
-    def job_operations(printer_status):
-        return rf'<div class="btn-group" role="group" aria-label="Basic checkbox toggle button group" style="height: 80px; width: 100%;"><input type="checkbox" onclick='"location.href='/action?btnregion=upper&btnaction=pause';"' class="btn-check" id="menu-upper-button-pause" autocomplete="off"><label class="btn btn-outline-danger" for="menu-upper-button-pause"><svg xmlns="http://www.w3.org/2000/svg" width="65" height="65" fill="currentColor" class="bi bi-pause-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M5 6.25a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0v-3.5zm3.5 0a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0v-3.5z"/></svg></label><input type="checkbox" onclick='"location.href='/action?btnregion=upper&btnaction=stop';"' class="btn-check" id="menu-upper-button-stop" autocomplete="off"><label class="btn btn-outline-danger" for="menu-upper-button-stop"><svg xmlns="http://www.w3.org/2000/svg" width="65" height="65" fill="currentColor" class="bi bi-stop-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M5 6.5A1.5 1.5 0 0 1 6.5 5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5v-3z"/></svg></label></div>'
+    def job_operations(menu_context_upper_btn_left = '', menu_context_upper_btn_right = ''):
+        return '<div class="btn-group" role="group" aria-label="Basic checkbox toggle button group" style="height: 80px; width: 100%;"><input type="checkbox" onclick='"location.href='/action?btnregion=upper&btnaction=pause';"' class="btn-check" id="menu-upper-button-pause" autocomplete="off"><label class="btn btn-outline-danger" for="menu-upper-button-pause">' + f'{menu_context_upper_btn_left}' + '</label><input type="checkbox" onclick='"location.href='/action?btnregion=upper&btnaction=stop';"' class="btn-check" id="menu-upper-button-stop" autocomplete="off"><label class="btn btn-outline-danger" for="menu-upper-button-stop">' + f'{menu_context_upper_btn_right}'+'</label></div>'
 
     def bed_clear():
         return '<label class="switch"><input type="checkbox"><span class="slider round"></span></label>'
@@ -15,10 +15,16 @@ class Buttons:
     
 class Views:    
     def status_display_upper(printer_status):
-        return f'<h2 class="display-2" style="font-weight:bold; font-size: 3rem;">{printer_status}</h2>'
+        return f'<h2 class="display-2"><span style="font-weight:bold; font-size: 3rem;">{printer_status}</span></h2>'
+
+    def status_display_upper_hybrid(printer_status, job_time):
+        return f'<h2 class="display-2" style="font-weight:bold; font-size: 3rem;">{printer_status}; {job_time}</h2>'
     
     def status_display_upper_emphasis(printer_status):
         return f'<h2 class="display-1" style="font-weight:bold; font-size: 6rem;">{printer_status}</h2>'
+
+    def status_display_upper_progress(job_progress):
+        return f'<progress id="file" max="100" value={job_progress} style="height: 100px; width: 100%; text-align: center;">{job_progress}</progress>'
     
     def status_display_lower_msg():
         return f'<h2 class="display-2" style="font-weight:bold; font-size: 3rem;">Please reconnect to OctoPrint.</h2>'
--- modulardisplay.py
+++ modulardisplay.py
@@ -1,10 +1,12 @@
 import requests 
 import json
 import os
+from math import floor
 from flask import Flask, render_template, request, url_for, redirect
 import datetime as dt
 from htmlbits import Buttons, Views
 from datamodels import DataModels
+from iconsbits import Bootstrap_SVG_Icons
 
 app=Flask(__name__)
 
@@ -17,7 +19,7 @@ btn_region_upper_actions_valid = ['pause', 'stop']
 btn_region_lower_actions_valid = ['connect', 'start']
 
 endpoint = "127.0.0.1:5000"
-X_Api_Key = "60F9D39D7BED47E793A89BA01156B141" #only works for local instance
+X_Api_Key = "593425E324D842BCB2938C7D8E583B76" #only works for local instance
 
 class printer_connection():
     def __init__(self, endpoint, X_Api_Key):
@@ -32,18 +34,15 @@ class printer_connection():
         command = btn_output['command']
         action = btn_output['action']
         printer_btn_ready = False
-        printer_btn_datatransfer = False
                 
         if btn_output != data_models.btn_output():
             printer_btn_ready = True
             if command != '' and action != '':
-                printer_btn_datatransfer = True
                 printer_btn_data = {
                     "command": command,
                     "action": action
                 }
             elif command != '' and action == '':
-                printer_btn_datatransfer = True
                 printer_btn_data = {
                     "command": command
                 }
@@ -215,6 +214,18 @@ class printer_connection():
         
         return bed_data_processed
     
+    def job_time_proc(self, job_time):
+        job_time = job_time
+
+        if floor(job_time/3600) >= 1:
+            job_time = f"{floor(float(job_time)/3600)} hr."
+        elif job_time/3600 <= 1 and floor(job_time/60) >= 1:
+            job_time = f"{floor(float(job_time)/60)} min."  
+        elif job_time/3600 <= 1 and job_time/60 < 1:
+            job_time = "< 1 min."   
+        
+        return job_time
+
     def job_data(self):
         job_data_raw = self.printer_job_get()
         job_data_processed = {}
@@ -224,13 +235,15 @@ class printer_connection():
         job_state = ''
         
         if job_data_raw != {}:
-            job_data_raw_job = job_data_raw['printer_job_get_job']
+            job_data_raw_job = job_data_raw['printer_job_get_job'] #12/08/22 -- currently unused (info will be needed with introduction of bed clearing and advanced job operations)
             job_data_raw_progress = job_data_raw['printer_job_get_progress']
             job_data_raw_state = job_data_raw['printer_job_get_state']
-            
-            job_time = job_data_raw_job['estimatedPrintTime']
-            job_progress = job_data_raw_progress['completion']
-            job_state = job_data_raw_state
+            try:
+                job_time = self.job_time_proc(float(job_data_raw_progress['printTimeLeft']))
+                job_progress = job_data_raw_progress['completion']
+                job_state = job_data_raw_state
+            except Exception:
+                pass
         else:
             pass
         
@@ -252,9 +265,9 @@ class printer_connection():
             if job_data != data_models.job_data():
                 if job_data['job_progress'] is not None:
                     if job_data['job_state'] != 'Paused':
-                        if job_data['job_progress'] <= 0.5:
+                        if job_data['job_progress'] <= 0.15:
                             return "Starting"
-                        elif job_data['job_progress'] > 0.5 and job_data['job_progress'] < 100:
+                        elif job_data['job_progress'] > 0.15 and job_data['job_progress'] < 100:
                             return "Printing"
                         elif job_data['job_progress'] == 100:
                             return "Complete"
@@ -270,28 +283,41 @@ class printer_connection():
             return "Offline"
 
 class component_sort():
-    def status_display_upper(self, printer_status):
+    def status_display_upper(self, printer_status, job_progress, job_time):
         status_upper = ""
+        status_upper_status = ""
+        status_upper_progress = ""
         views = Views
         
         if printer_status in activity_state_1 or printer_status in activity_state_2:
-            status_upper = views.status_display_upper_emphasis(printer_status)
-        elif printer_status in activity_state_3 or printer_status in activity_state_4:
-            status_upper = views.status_display_upper(printer_status)
+            status_upper_status = views.status_display_upper_emphasis(printer_status)
+            status_upper_progress = ""
+        elif printer_status in activity_state_3:
+            status_upper_status = views.status_display_upper_hybrid(printer_status, job_time)
+            status_upper_progress = views.status_display_upper_progress(job_progress)
+        elif printer_status in activity_state_4:
+            status_upper_status = views.status_display_upper(printer_status)
+            status_upper_progress = views.status_display_upper_progress(job_progress)
+
+        status_upper = status_upper_status + status_upper_progress + "<hr>"
         return status_upper
 
     def menu_context_upper(self, printer_status):
         menu_upper = ""
         printer_status=printer_status
         buttons = Buttons
+        icons = Bootstrap_SVG_Icons
 
         if printer_status in activity_state_1:
             menu_upper = "" #10-22-22: remains empty, devoid of light
         elif printer_status in activity_state_2 or printer_status in activity_state_4:
             menu_upper = buttons.bed_clear() #10-22-22: change to bed clear toggle
         elif printer_status in activity_state_3:
-            menu_upper = buttons.job_operations(printer_status=printer_status) #10-22-22: change to job operations shortcuts    
-        return menu_upper
+            if printer_status == "Paused":
+                menu_upper = buttons.job_operations(menu_context_upper_btn_left=icons.icon_svg_play(), menu_context_upper_btn_right=icons.icon_svg_stop()) #10-22-22: change to job operations shortcuts   
+            else: 
+                menu_upper = buttons.job_operations(menu_context_upper_btn_left=icons.icon_svg_pause(), menu_context_upper_btn_right=icons.icon_svg_stop())
+        return menu_upper + "<hr>"
 
     def status_display_lower(self, printer_status, 
                             ext_temp_actual, 
@@ -359,7 +385,7 @@ def display_final():
                                             job_data=job_data)
 
     components = component_sort()
-    status_display_upper_data = components.status_display_upper(printer_status=printer_status)
+    status_display_upper_data = components.status_display_upper(printer_status=printer_status, job_progress=job_data['job_progress'], job_time=job_data['job_time'])
     menu_context_upper_data = components.menu_context_upper(printer_status=printer_status)
     status_display_lower_data = components.status_display_lower(printer_status=printer_status, 
                                                     ext_temp_actual=ext_data["ext_temp_actual"],
@@ -372,7 +398,8 @@ def display_final():
                                                     bed_temp_state=bed_data["bed_temp_state"])
     menu_context_lower_data = components.menu_context_lower(printer_status=printer_status)
 
-    return render_template("test_display.html", 
+    return render_template("test_display.html",
+                           raw_data_diagnostic=(ext_data,bed_data,job_data,printer_status),
                            status_display_upper=status_display_upper_data, 
                            menu_context_upper=menu_context_upper_data, 
                            status_display_lower=status_display_lower_data,
